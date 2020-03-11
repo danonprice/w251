1d3961549a8a2cea072c02b3babe4074ee37e109
Dan <danonprice@gmail.com>
2020-03-11 07:13:04 -0700

    broker

--- homework/week03/launch_face_detector_tx2.sh
+++ homework/week03/launch_face_detector_tx2.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Create a bridge:
-docker network create --driver bridge face_detect_net
+docker network create --driver bridge face_detect_net &
 echo "created network bridge"
 
 # run mqtt broker
 #docker run --name mqtt_broker_0 --network face_detect_net -p 1883:1883 -ti mqtt_broker
-docker run --network face_detect_net -p 1883:1883 -ti mqtt_broker
+docker run --network face_detect_net -p 1883:1883 -ti mqtt_broker &
 echo "running broker"
+
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' <container_id>
 
 # run mqtt message forwarder
 #docker run --name mqtt_forwarder_0 --network face_detect_net -ti mqtt_forwarder
-docker run --network face_detect_net -ti mqtt_forwarder
+docker run --network face_detect_net -ti mqtt_forwarder &
 echo "running forwarder"
 
 # run face detector
 xhost +
-docker run -e DISPLAY=$DISPLAY --rm --privileged -v /tmp:/tmp -ti face_detector
+docker run -e DISPLAY=$DISPLAY --rm --privileged -v /tmp:/tmp -ti face_detector &
