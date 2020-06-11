bc83896bec22c3ca2c594544b5b350569aa0418d
Dan Price <dan.price@berkeley.edu>
2020-06-10 21:08:16 -0700

    s3fs

--- homework/week03/launch_image_processor_cloud.sh
+++ homework/week03/launch_image_processor_cloud.sh
@@ -8,3 +8,5 @@ docker run --name mqtt_broker_0 --network face_detect_net -p 1883:1883 -ti mqtt_
 
 # run image processor
 docker run --name image_processor_0 --network face_detect_net -p 1883:1883 -ti image_processor
+
+s3fs w251-face-app /tmp/face -o passwd_file=$HOME/.cos_creds -o sigv2 -o use_path_request_style -o url=https://s3.us-east.objectstorage.softlayer.net
