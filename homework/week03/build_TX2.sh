f43346457082dd6fd319603cd424791b92a9d7c0
danonprice <danonprice@users.noreply.github.com>
2020-06-12 17:05:42 -0700

    build containers shell script

---
+++ homework/week03/build_TX2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create a bridge:
+docker network create --driver bridge face_detect_net &
+echo "Created network bridge"
+
+cd mqtt_broker
+sudo docker build -t mqtt_broker -f Dockerfile.mqtt_broker .
+
+cd ../mqtt_forwarder
+sudo docker build -t mqtt_forwarder -f Dockerfile.mqtt_forwarder .
+
+cd ../face_detector
+sudo docker build -t face_detector -f Dockerfile.face_detector .
---
+++ homework/week03/build_cloud.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create a bridge:
+docker network create --driver bridge face_detect_net &
+echo "Created network bridge"
+
+cd mqtt_broker
+docker build -t mqtt_broker -f Dockerfile.mqtt_broker .
+
+cd ../image_processor
+docker build -t image_processor -f Dockerfile.image_processor .
+
+echo "Built cloud MQTT broker and Image Processor"
