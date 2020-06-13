76ac43e54d8f646e2e9cc853811d20fecee845bb
danonprice <danonprice@users.noreply.github.com>
2020-06-12 17:13:18 -0700

    organization

--- homework/week03/launch_image_processor_cloud.sh
+++ homework/week03/launch_image_processor_cloud.sh
@@ -4,9 +4,9 @@
 docker network create --driver bridge face_detect_net
 
 # run mqtt broker
-docker run --name mqtt_broker_0 --network face_detect_net -p 1883:1883 -ti mqtt_broker
+docker run -d --name mqtt_broker_remote --network face_detect_net -p 1883:1883 mqtt_broker
 
 # run image processor
-docker run --name image_processor_0 --network face_detect_net -p 1883:1883 -ti image_processor
+docker run --name image_processor_remote --network face_detect_net -p 1883:1883 -ti image_processor
 
-s3fs w251-face-app /tmp/face -o passwd_file=$HOME/.cos_creds -o sigv2 -o use_path_request_style -o url=https://s3.us-east.objectstorage.softlayer.net
+#s3fs w251-face-app /tmp/face -o passwd_file=$HOME/.cos_creds -o sigv2 -o use_path_request_style -o url=https://s3.us-east.objectstorage.softlayer.net
