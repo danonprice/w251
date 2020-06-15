0e3a22b16cd9b52912c2e55a7394478434384f20
Dan <danonprice@gmail.com>
2020-06-15 00:33:25 -0700

    tx2 cleanup

--- homework/week03/mqtt_forwarder/forward.py
+++ homework/week03/mqtt_forwarder/forward.py
@@ -1,52 +1,46 @@
 import paho.mqtt.client as mqtt
 
+
+REMOTE_MQTT_HOST = "192.168.1.24"
+LOCAL_MQTT_HOST = "mqtt_broker_tx2"
+MQTT_PORT = 1883
 MQTT_TOPIC = "tx2/face"
 QOS = 2
 
-LOCAL_MQTT_HOST = "172.18.0.2"
-MQTT_PORT = 1883
-
-REMOTE_MQTT_HOST = "192.168.1.24" #"172.19.0.2"
-
 def on_connect_local(client, userdata, flags, rc):
-    print("connected to local broker with rc: " + str(rc))
     client.subscribe(MQTT_TOPIC)
-
-def on_subscribe(client, userdata, msgid, qos):
-    print("subscribed to topic ",MQTT_TOPIC)
+    print("Connected to",LOCAL_MQTT_HOST,"with result code",str(rc))
+    print("Subscribed to topic",MQTT_TOPIC)
 
 def on_connect_remote(client, userdata, flags, rc):
-    print("connected to remote broker with rc: " + str(rc))
-    #client.subscribe(MQTT_TOPIC)
+    print("Connected to",REMOTE_MQTT_HOST,"with result code",str(rc))
 
 def on_message(client,userdata, msg):
-  global remote_mqttclient
-  try:
-    print("message received - ",msg.topic)
-    remote_mqttclient.publish(msg.topic, payload=msg.payload, qos=QOS, retain=False)
-  except:
-    print("unexpected error:", sys.exc_info()[0])
+    global remote_mqttclient
+    try:
+        print("Message received on topic",msg.topic)
+        remote_mqttclient.publish(msg.topic, payload=msg.payload, qos=QOS, retain=False)
+    except:
+        print("Message error:", sys.exc_info()[0])
 
 def on_publish(client, userdata, msgid):
-    print("message published to remote server - ",msgid)
+    print("Message",msgid,"published to remote server",REMOTE_MQTT_HOST)
 
 def on_disconnect_local(client, userdata, rc):
     client.loop_stop()
 
 
 local_mqttclient = mqtt.Client("TX2-Forward")
-remote_mqttclient = mqtt.Client("Remote-Forward")
-
 local_mqttclient.on_connect = on_connect_local
-local_mqttclient.on_subscribe = on_subscribe
-remote_mqttclient.on_connect = on_connect_remote
-
 local_mqttclient.on_message = on_message
-remote_mqttclient.on_publish = on_publish
 local_mqttclient.on_disconnect = on_disconnect_local
+
+remote_mqttclient = mqtt.Client("Remote-Forward")
+remote_mqttclient.on_connect = on_connect_remote
+remote_mqttclient.on_publish = on_publish
 
 local_mqttclient.connect(LOCAL_MQTT_HOST, MQTT_PORT, 600)
 local_mqttclient.loop_start()
-remote_mqttclient.connect(REMOTE_MQTT_HOST, MQTT_PORT, 600)
 
-remote_mqttclient.loop_forever()  
+remote_mqttclient.connect(REMOTE_MQTT_HOST, MQTT_PORT, 600)
+remote_mqttclient.loop_forever()
